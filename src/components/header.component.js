efa90259940e2b170a76ca4046d724e7097a5fc7
Vit Onischenko <slonik0775@gmail.com>
2022-05-11 01:08:32 +0300

    Working tabs

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -6,6 +6,9 @@ constructor(id){
 }
 
 init() {
+    if(localStorage.getItem('visited')){
+        this.hide()
+    }
   const btn = this.$el.querySelector('.js-header-start')
 btn.addEventListener('click', buttonHandler.bind(this) )
 }
@@ -13,5 +16,7 @@ btn.addEventListener('click', buttonHandler.bind(this) )
 
 
 function buttonHandler(){
+  
+    localStorage.setItem('visited', JSON.stringify(true))
 this.hide()
 }
